5e0e1c57bdd615df658d3c2f0d6d4f45e90f211a
n3xistence <maxinies00@gmail.com>
2023-10-05 16:48:49 +0200

    Set up Jest

---
+++ tests/init.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from "@jest/globals";
+import * as helper from "../src/ext/Helper";
+
+const userID = "";
+
+describe("init", () => {
+  test("Initializing...", () => {
+    expect(1).toBe(1);
+  });
+});
